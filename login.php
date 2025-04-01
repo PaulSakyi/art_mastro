48b8cb0ac290ad8e45bf15f620baa852990c6a29
PaulSakyi <paulsak123@gmail.com>
2025-04-01 04:28:49 +0000

    Fixed login redirection and ensured PHP files open as pages

--- login.php
+++ login.php
@@ -14,35 +14,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $error = "Invalid credentials!";
     }
 }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<h2>Login</h2>
-
-<?php
-if (isset($error)) {
-    echo "<p style='color: red;'>$error</p>";
-}
-?>
-
-<div class="login-form">
-    <form method="POST" action="login.php">
-        <label for="username">Username: </label>
-        <input type="text" name="username" id="username" required><br>
-        <label for="password">Password: </label>
-        <input type="password" name="password" id="password" required><br>
-        <button type="submit">Login</button>
-    </form>
-</div>
-
-</body>
-</html>
+?> 
+ 
