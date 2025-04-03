91798aa05aa5916326a85f64a192594daa062391
PaulSakyi <paulsak123@gmail.com>
2025-04-03 01:31:53 +0000

    Some files deleted

--- gallery.php
+++
@@ -1,8 +0,0 @@
-<?php
-$files = scandir("uploads/");
-foreach ($files as $file) {
-    if ($file !== "." && $file !== "..") {
-        echo "<img src='uploads/$file' alt='$file' class='gallery-item'>";
-    }
-}
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Log out and destroy session
-header("Location: login.php"); // Redirect to login page after logout
-exit;
-?>
